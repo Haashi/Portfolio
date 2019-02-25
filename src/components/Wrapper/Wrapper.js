dd489afb416ff649d4862097f30be7dd5f418c67
Haashi <baptiste.pontanier@laposte.net>
2019-02-25 15:26:39 +0100

    wrapper using extended info

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -4,20 +4,23 @@ import { translate } from "react-i18next";
 import Info from "../Info/Info";
 import Edubg from "../Edubg/Edubg";
 import Workexp from "../Workexp/Workexp";
+import Home from "../Home/Home";
 import "./Wrapper.css";
 
 class Wrapper extends Component {
   render() {
+    console.log(this.props.location.pathname);
     return (
       <div
         className="w3-content w3-margin-top w3-margin-bottom wrapper anim"
         style={{ maxWidth: "1400px" }}
       >
         <div className="w3-row-padding">
-          <Info />
+          {this.props.location.pathname === "/" ? <Info /> : <Info expanded />}
           <Switch location={this.props.location}>
             <Route exact path="/edubg" component={Edubg} />
             <Route exact path="/workexp" component={Workexp} />
+            <Route exact path="/" component={Home} />
           </Switch>
         </div>
       </div>
