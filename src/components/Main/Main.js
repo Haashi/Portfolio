ca28578028732b342870056ec2b16d3e69e5f74b
Haashi <baptiste.pontanier@laposte.net>
2019-02-25 15:26:04 +0100

    all routing to wrapper

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Route, Switch, withRouter } from "react-router-dom";
 import { translate } from "react-i18next";
-import Home from "../Home/Home";
 import Wrapper from "../Wrapper/Wrapper";
 import Projects from "../Projects/Projects";
 import "./Main.css";
@@ -10,7 +9,6 @@ class Main extends Component {
   render() {
     return (
       <Switch location={this.props.location}>
-        <Route exact path="/" component={Home} />
         <Route exact path="/(edubg|workexp|)/" component={Wrapper} />
         <Route exact path="/projects" component={Projects} />
       </Switch>
