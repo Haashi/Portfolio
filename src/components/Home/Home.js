a2c22b3c0ac5a9629741117380f9508ff8b51f86
Haashi <baptiste.pontanier@laposte.net>
2019-02-25 15:26:51 +0100

    two third on home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,33 +6,33 @@ import "./Home.css";
 class Home extends Component {
   render() {
     const { t } = this.props;
-
     const welcome = (
-      <div className="w3-container w3-card w3-white w3-margin-bottom w3-margin-left w3-margin-right">
-        <div className="jumbotron w3-center">
-          <h1 className="display-4">
-            {t("home.welcome", { framework: "react-i18next" })}
-          </h1>
-          <p className="lead">{t("home.me", { framework: "react-i18next" })}</p>
-          <br />
-          <hr className="my-4" />
-          <br />
-          <p>{t("home.description1", { framework: "react-i18next" })}</p>
-          <p>{t("home.description2", { framework: "react-i18next" })}</p>
-          <p>{t("home.description3", { framework: "react-i18next" })}</p>
-          <Link className="btn btn-secondary btn-lg" to="/edubg" role="button">
-            {t("home.learnmore", { framework: "react-i18next" })}
-          </Link>
-        </div>
+      <div className="jumbotron w3-center">
+        <h1 className="display-4">
+          {t("home.welcome", { framework: "react-i18next" })}
+        </h1>
+        <p className="lead">{t("home.me", { framework: "react-i18next" })}</p>
+        <br />
+        <hr className="my-4" />
+        <br />
+        <p>{t("home.description1", { framework: "react-i18next" })}</p>
+        <p>{t("home.description2", { framework: "react-i18next" })}</p>
+        <p>{t("home.description3", { framework: "react-i18next" })}</p>
+        <Link className="btn btn-secondary btn-lg" to="/edubg" role="button">
+          {t("home.learnmore", { framework: "react-i18next" })}
+        </Link>
       </div>
     );
 
     return (
-      <div
-        className="w3-content w3-margin-top w3-margin-bottom home anim"
-        style={{ maxWidth: "1400px" }}
-      >
-        <div className="w3-row-padding">{welcome}</div>
+      <div>
+        <div //using twothird because info component use the w3-third
+          className="w3-twothird edubg anim"
+        >
+          <div className="w3-container w3-card w3-white w3-margin-bottom w3-margin-left">
+            {welcome}
+          </div>
+        </div>
       </div>
     );
   }
