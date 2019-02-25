d2b16ed0e33c7deb8fca98105e6aebca6270ade9
Haashi <baptiste.pontanier@laposte.net>
2019-02-25 15:26:15 +0100

    reworked info bar

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { translate } from "react-i18next";
+import "./Info.css";
 
 class Info extends Component {
   render() {
     const { t } = this.props;
-    console.log();
+
     return (
-      <div
-        className={
-          this.props.location.pathname.startsWith("/edubg")
-            ? "w3-third"
-            : "w3-third w3-hide-small"
-        }
-      >
+      <div className="w3-third">
         <div className="w3-white w3-text-grey w3-card-4 w3-margin-bottom w3-margin-left">
           <div className="w3-display-container">
             <img src="profile.jpg" style={{ width: "100%" }} alt="Avatar" />
@@ -22,53 +17,60 @@ class Info extends Component {
             <hr />
             <h2>Baptiste "Haashi" Pontanier</h2>
             <hr />
-            <p>
-              <i className="fa fa-briefcase fa-fw w3-margin-right w3-large w3-text-light-green" />
-              {t("info.current", { framework: "react-i18next" })}
-            </p>
-            <p>
-              <i className="fa fa-home fa-fw w3-margin-right w3-large w3-text-light-green" />
-              {t("info.place", { framework: "react-i18next" })}
-            </p>
-            <p>
-              <i className="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-light-green" />
-              baptiste.pontanier@laposte.net
-            </p>
-            <p>
-              <i className="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-light-green" />
-              baptiste.pontanier@etu.esisar.grenoble-inp.fr
-            </p>
-            <p>
-              <i className="fa fa-phone fa-fw w3-margin-right w3-large w3-text-light-green" />
-              +33642123867
-            </p>
-            <hr />
-            <p className="w3-large w3-text-theme">
-              <b>
-                <i className="fa fa-globe fa-fw w3-margin-right w3-text-light-green" />
-                {t("info.languages.tag", { framework: "react-i18next" })}
-              </b>
-            </p>
-            <p>{t("info.languages.fr", { framework: "react-i18next" })}</p>
-            <div className="w3-light-grey w3-round-xlarge">
+            {this.props.expanded ? (
               <div
-                className="w3-round-xlarge w3-light-green"
-                style={{ height: "24px", width: "100%" }}
-              />
-            </div>
-            <br />
-            <p>
-              {t("info.languages.en", { framework: "react-i18next" })} (TOEIC
-              Score : 965)
-            </p>
-            <div className="w3-light-grey w3-round-xlarge">
-              <div
-                className="w3-round-xlarge w3-light-green"
-                style={{ height: "24px", width: "80%" }}
-              />
-            </div>
-            <br />
-            <hr />
+                className={
+                  "w3-animate-top" +
+                  (this.props.location.pathname.startsWith("/edubg")
+                    ? ""
+                    : " w3-hide-small")
+                }
+              >
+                <p>
+                  <i className="fa fa-briefcase fa-fw w3-large w3-text-light-green" />
+                  {t("info.current", { framework: "react-i18next" })}
+                </p>
+                <p>
+                  <i className="fa fa-home fa-fw w3-large w3-text-light-green" />
+                  {t("info.place", { framework: "react-i18next" })}
+                </p>
+                <p>
+                  <i className="fa fa-envelope fa-fw w3-large w3-text-light-green" />
+                  baptiste.pontanier@laposte.net
+                </p>
+                <p>
+                  <i className="fa fa-phone fa-fw w3-large w3-text-light-green" />
+                  +33642123867
+                </p>
+                <hr />
+                <p className="w3-large w3-text-theme">
+                  <b>
+                    <i className="fa fa-globe fa-fw w3-text-light-green" />
+                    {t("info.languages.tag", { framework: "react-i18next" })}
+                  </b>
+                </p>
+                <p>{t("info.languages.fr", { framework: "react-i18next" })}</p>
+                <div className="w3-light-grey w3-round-xlarge">
+                  <div
+                    className="w3-round-xlarge w3-light-green"
+                    style={{ height: "24px", width: "100%" }}
+                  />
+                </div>
+                <br />
+                <p>
+                  {t("info.languages.en", { framework: "react-i18next" })}{" "}
+                  (TOEIC Score : 965)
+                </p>
+                <div className="w3-light-grey w3-round-xlarge">
+                  <div
+                    className="w3-round-xlarge w3-light-green"
+                    style={{ height: "24px", width: "80%" }}
+                  />
+                </div>
+                <br />
+                <hr />
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
